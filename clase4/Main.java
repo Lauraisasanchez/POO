d4758eebf1cc85d046c36f1785063c571b093d39
Lauraisasanchez <142424929+Lauraisasanchez@users.noreply.github.com>
2023-09-13 23:12:35 -0500

    Update Main.java

--- clase4/Main.java
+++ clase4/Main.java
@@ -9,19 +9,19 @@ public class Main {
 	    animal perro = new perro("Stich","Carnivoro",15,"Doberman");
         perro.Alimentarse();
         //-->Declaracion de otro objeto PERRO
-        perro perro1 = new perro("Teddy","Croquetas",10,"Chihuahua");
+        perro perro1 = new perro("firulais","Croquetas",10,"Chihuahua");
         perro1.Alimentarse();
         //-->Declaracion del objeto Gato
-        animal gato = new gato("Pelusa","Galletas",15,"Siames");
+        animal gato = new gato("ferxxoo","bandejaPaisa",15,"Siames");
         gato.Alimentarse();
         //-->Declaracion del objeto Caballo
-        animal caballo = new caballo("juan","Pasto",25,"Fino");
+        animal caballo = new caballo("juan","chontaduro",25,"Fino");
         caballo.Alimentarse();
         //delaracion de objeto araña
-        animal araña = new araña("faber","fourloko",18,"tarantula" );
+        animal araña = new araña("faber","salchipapa",18,"tarantula" );
         araña.Alimentarse();
         //declaracion del objeto sapo 
-        animal sapo = new sapo("sebacho","gusanos",17,"feka" );
+        animal sapo = new sapo("sebacho","fourloko",17,"feka" );
         sapo.Alimentarse();
     }
 }
